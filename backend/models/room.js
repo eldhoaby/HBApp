41c652a4953be9202c8a01b61c927ceac8679e1a
Eldho <eldhoaby10@gmail.com>
2025-06-30 22:18:06 +0530

    Logo added ...

--- backend/models/room.js
+++ backend/models/room.js
@@ -1,3 +1,27 @@
+// import mongoose from "mongoose";
+
+// const roomSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   city: { type: String, required: true },
+//   address: { type: String, required: true },
+//   maxCount: { type: Number, required: true },
+//   phoneNumber: { type: String, required: true },
+//   amenities: { type: [String], required: true },
+//   price: { type: Number, required: true },
+//   rating: { type: Number, required: true },
+//   reviewsCount: { type: Number, required: true },
+//   images: { type: [String], required: true }, // array of image URLs
+//   roomType: { type: String, required: true }, // e.g. "Luxury Room"
+//   owner: {
+//     name: String,
+//     image: String
+//   }
+// });
+
+// const Room = mongoose.model("Room", roomSchema);
+// export default Room;
+
+// models/room.js
 import mongoose from "mongoose";
 
 const roomSchema = new mongoose.Schema({
@@ -10,8 +34,8 @@ const roomSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   rating: { type: Number, required: true },
   reviewsCount: { type: Number, required: true },
-  images: { type: [String], required: true }, // array of image URLs
-  roomType: { type: String, required: true }, // e.g. "Luxury Room"
+  images: { type: [String], required: true },
+  roomType: { type: String, required: true },
   owner: {
     name: String,
     image: String
